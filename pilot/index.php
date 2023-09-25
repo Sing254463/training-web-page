45152d349844f927ce6816e056f5c6a7267be21c
Sing254463 <139378006+Sing254463@users.noreply.github.com>
2023-09-25 12:36:29 +0700

    Add files via upload

---
+++ pilot/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>space</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="navbar">
+
+    </div>
+    <div class="text_heat">
+        <p>Pilot</p></div>
+    
+    <div class="img">
+        <img src="img\space_pilot.jpg" alt="space_pilot">
+    </div>
+    <div class="manu">
+
+    </div>
+    <div class="continue">
+        <h1>Unlock freedom</h1>
+        <h1>with going to space</h1>
+        <div class="text2">
+        <p>Are you ready to explore the vast, limitless </p>
+        <p>With your astronaut yet?</p>
+        <p>No need to keep looking, pilot is your destination to </p>
+        <p>explore space without limits.</p><br>
+
+        <p> No matter who you are,  </p>
+        <p> Astronaut will take you on an experience.  </p>
+        <p> with endless exploration of the stars  </p>
+        <p> And everything will benefit you. </p>
+        </div>
+    </div>
+    <div class="img2">
+        <img src="img\2.png" alt="2">
+    </div>
+
+    <div class="img1">
+        <img src="img\1.png" alt="1">
+    </div>
+    <div class="img3">
+    </div>
+
+
+</body>
+</html>
